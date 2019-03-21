85dedc24200af528359568ef1f99ebfe5a5e8fd4
annettethomas <42778997+annettethomas@users.noreply.github.com>
2019-03-21 11:52:58 -0600

    bookreview (part 1)

--- books/app.py
+++ books/app.py
@@ -7,5 +7,57 @@ app = Flask(__name__)
 def books():
     return render_template('index.html')
 
+
+@app.route('/artbooks')
+def artbooks():
+    return render_template('artbooks.html')
+
+@app.route('/artdesc1')
+def artdesc1():
+    return render_template('artdesc1.html')
+
+@app.route('/artdesc2')
+def artdesc2():
+    return render_template('artdesc2.html')
+
+@app.route('/artdesc3')
+def artdesc3():
+    return render_template('artdesc3.html')
+
+
+@app.route('/comics')
+def comics():
+    return render_template('comics.html')
+
+@app.route('/comicsdesc1')
+def comicdesc1():
+    return render_template('comicsdesc1.html')
+
+@app.route('/comicsdesc2')
+def comicdesc2():
+    return render_template('comicsdesc2.html')
+
+@app.route('/comicsdesc3')
+def comicdesc3():
+    return render_template('comicsdesc3.html')
+
+
+
+@app.route('/fantasy')
+def fantasy():
+    return render_template('fantasy.html')
+
+@app.route('/fantasydesc1')
+def fantasydesc1():
+    return render_template('fantasydesc1.html')
+
+@app.route('/fantasydesc2')
+def fantasydesc2():
+    return render_template('fantasydesc2.html')
+
+@app.route('/fantasydesc3')
+def fantasydesc3():
+    return render_template('fantasydesc3.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
